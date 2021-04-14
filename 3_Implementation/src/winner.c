2d1a86617c5a4b306301c96e2b28e6ce2bbffb9d
256274 <sumanthreddy83@gmail.com>
2021-04-14 15:58:11 +0530

    made few changes

--- 3_Implementation/src/winner.c
+++
@@ -1,9 +0,0 @@
-#include "tic_tac_toe.h"
-
-void Declare_Winner(int Turn){
-    if (Turn == CPU)
-        printf("COMPUTER won the Match\n\n");
-    else
-        printf("User won the Match!!!!\n\n");
-    return;
-}
